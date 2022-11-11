fa58524e91e01e2d2c4befde44c60a192bf6de4a
paptriana <117661433+paptriana@users.noreply.github.com>
2022-11-10 22:04:14 -0500

    Update ControladorResultado.py
    Se modificó todo el documento

--- be_flask/Controladores/ControladorResultado.py
+++ be_flask/Controladores/ControladorResultado.py
@@ -1,25 +1,42 @@
-from Repositorios.RepositorioResultado import RepositorioResultado
 from Modelos.Resultado import Resultado
+from Modelos.Mesa import Mesa
+from Modelos.Candidato import Candidato
+from Repositorios.RepositorioResultado import RepositorioResultado
+from Repositorios.RepositorioCandidato import RepositorioCandidato
+from Repositorios.RepositorioMesa import RepositorioMesa
 
 
 class ControladorResultado():
     def __init__(self):
         self.repositorioResultado = RepositorioResultado()
+        self.repositorioCandidato = RepositorioCandidato()
+        self.repositorioMesa = RepositorioMesa()
 
     def index(self):
         return self.repositorioResultado.findAll()
 
-    def create(self, infoResultado):
-        nuevoResultado= Resultado(infoResultado)
+# Asignar Candidato y mesa a Resultado
+
+    def create(self, infoResultado, id_candidato, id_mesa):
+        nuevoResultado = Resultado(infoResultado)
+        elCandidato = Candidato(self.repositorioCandidato.findById(id_candidato))
+        laMesa = Mesa(self.repositorioMesa.findById(id_mesa))
+        nuevoResultado.candidato = elCandidato
+        nuevoResultado.mesa = laMesa
         return self.repositorioResultado.save(nuevoResultado)
 
     def show(self, id):
         elResultado = Resultado(self.repositorioResultado.findById(id))
         return elResultado.__dict__
 
-    def update(self, id, infoResultado):
-        resultadoActual = Resultado(self.repositorioResultado.findById(id))
-        return self.repositorioResultado.save(resultadoActual)
+    def update(self, id, infoResultado, id_candidato, id_mesa):
+        elResultado = Resultado(self.repositorioResultado.findById(id))
+        elCandidato = Candidato(self.repositorioCandidato.findById(id_candidato))
+        laMesa = Mesa(self.repositorioMesa.findById(id_mesa))
+        elResultado.mesa = laMesa
+        elResultado.candidato = elCandidato
+        elResultado.numero_votos = infoResultado["numero_votos"]
+        return self.repositorioResultado.save(elResultado)
 
     def delete(self, id):
         return self.repositorioResultado.delete(id)
