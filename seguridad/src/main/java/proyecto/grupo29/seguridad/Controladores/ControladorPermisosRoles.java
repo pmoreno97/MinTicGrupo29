c2cd2c3f597ed014e0488410a3b81ed256226d09
Pablo Andrés Moreno Alvarez <96327216+PabloAMoreno97@users.noreply.github.com>
2022-11-16 13:20:50 -0500

    Creación de la relación entre el permiso y los roles

--- seguridad/src/main/java/proyecto/grupo29/seguridad/Controladores/ControladorPermisosRoles.java
+++ seguridad/src/main/java/proyecto/grupo29/seguridad/Controladores/ControladorPermisosRoles.java
@@ -54,6 +54,14 @@ public class ControladorPermisosRoles {
         }
     }
 
+    /**
+     *
+     * @param id
+     * @param id_rol
+     * @param id_permiso
+     * @param infoPermisoRole
+     * @return
+     */
     @PutMapping("{id}/rol/{id_rol}/permiso/{id_permiso}")
     public PermisosRoles create(@PathVariable String id, @PathVariable String id_rol, @PathVariable String id_permiso, @RequestBody PermisosRoles infoPermisoRole){
         PermisosRoles permisoRolActual = this.miRepositorioPermisosRoles.findById(id).orElse(null);
