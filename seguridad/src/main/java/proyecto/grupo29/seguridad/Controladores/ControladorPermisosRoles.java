516d037541b0ceece22bd6e8bd0d79a25feb498c
paptriana <paptriana@gmail.com>
2022-11-25 00:37:26 -0500

    Se creó el metodo para validar usuario

--- seguridad/src/main/java/proyecto/grupo29/seguridad/Controladores/ControladorPermisosRoles.java
+++ seguridad/src/main/java/proyecto/grupo29/seguridad/Controladores/ControladorPermisosRoles.java
@@ -85,5 +85,20 @@ public class ControladorPermisosRoles {
         }
     }
 
+    @GetMapping("validar-permiso/rol/{id_rol}")
+    public PermisosRoles getPermiso(@PathVariable String id_rol,@RequestBody
+    Permiso infoPermiso){
+        Permiso elPermiso=this.miRepositorioPermiso
+                .getPermiso(infoPermiso.getUrl(),
+                        infoPermiso.getMetodo());
+        Rol elRol=this.miRepositorioRol.findById(id_rol).get();
+        if (elPermiso!=null && elRol!=null){
+            return
+                    this.miRepositorioPermisosRoles.getPermisoRol(elRol.get_id(),elPermiso.get_id());
+        }else{
+            return null;
+        }
+    }
+
 
 }
--- seguridad/src/main/java/proyecto/grupo29/seguridad/Repositorios/RepositorioPermisosRoles.java
+++ seguridad/src/main/java/proyecto/grupo29/seguridad/Repositorios/RepositorioPermisosRoles.java
@@ -1,7 +1,10 @@
 package proyecto.grupo29.seguridad.Repositorios;
 
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import proyecto.grupo29.seguridad.Modelos.PermisosRoles;
 
 public interface RepositorioPermisosRoles extends MongoRepository<PermisosRoles, String> {
+    @Query("{'rol.$id': ObjectId(?0),'permiso.$id': ObjectId(?1)}")
+    PermisosRoles getPermisoRol(String id_rol,String id_permiso);
 }
