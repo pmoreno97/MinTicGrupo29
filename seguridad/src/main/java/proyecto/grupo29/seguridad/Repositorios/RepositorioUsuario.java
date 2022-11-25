6ecf20e754e23374eecda5faae3c81aac231be59
paptriana <paptriana@gmail.com>
2022-11-24 23:25:02 -0500

    prueba

--- seguridad/src/main/java/proyecto/grupo29/seguridad/Repositorios/RepositorioUsuario.java
+++ seguridad/src/main/java/proyecto/grupo29/seguridad/Repositorios/RepositorioUsuario.java
@@ -1,7 +1,10 @@
 package proyecto.grupo29.seguridad.Repositorios;
 
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import proyecto.grupo29.seguridad.Modelos.Usuario;
 
 public interface RepositorioUsuario extends MongoRepository<Usuario, String> {
+    @Query("{'correo': ?0}")
+    public Usuario getUserByEmail(String correo);
 }
